5f0c22a7582e940ed2f112358f52e13cb652155d
t0dd <swjasontodd@gmail.com>
2022-01-11 15:56:22 +0900

    reset project with tailwindcss

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    theme: 'light',
+    language: '',
+    color: {
+      primary: '#000',
+      secondary: '#fff',
+    }
   },
   mutations: {
   },
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { useMouse, usePreferredDark, useLocalStorage } from '@vueuse/core'
+
+const { x, y } = useMouse()
+
+// is user prefers dark theme
+const isDark = usePreferredDark()
+
+// persist state in localStorage
+const store = useLocalStorage(
+  'my-storage', 
+  {
+    name: 'Apple',
+    color: 'red',
+  },
+)
+
 </script>
 
 <template>
-
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  {{ x }} {{ y }} {{ store.name }}
 </template>
 
 <style scoped lang="scss">
